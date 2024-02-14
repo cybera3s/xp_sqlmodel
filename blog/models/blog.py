0183d476f267bff6218d69b8461abd5724a0122f
ario <cybera.3s@gmail.com>
2024-02-14 17:09:17 +0330

    feat: integrate alembic

--- blog/models/blog.py
+++ blog/models/blog.py
@@ -7,3 +7,9 @@ class Post(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     title: str = Field(unique=True)
     content: str
+    views_count: int | None = Field(default=None)
+
+
+class Comment(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    body: str
